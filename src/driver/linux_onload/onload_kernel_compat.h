ol-alexandra/onload

commit af4d93c259102755f44a6092d8b344ee7441090a

ON-16108: kernel compat for f_owner type on 6.12

src/driver/linux_onload/onload_kernel_compat.h
@@ -115,5 +115,15 @@ static inline int efrm_unregister_netdevice_notifier(struct notifier_block *b)
 #define unregister_netdevice_notifier efrm_unregister_netdevice_notifier
 #endif
 
+static inline struct fown_struct* efrm_file_f_owner(struct file *file)
+{
+#ifdef EFRM_F_OWNER_IS_VAL
+	return &file->f_owner;
+#else
+	/* linux 6.12+ */
+	return file->f_owner;
+#endif
+}
+
 
 #endif /* __ONLOAD_KERNEL_COMPAT_H__ */

src/driver/linux_onload/ossock_calls.c
@@ -657,14 +657,15 @@ int efab_tcp_helper_create_os_sock(ci_private_t *priv)
 
   /* Copy F_SETOWN_EX, F_SETSIG to the new file */
 #ifdef F_SETOWN_EX
-  if(priv->_filp->f_owner.pid != 0) {
+  if(efrm_file_f_owner(priv->_filp)->pid != 0) {
     rcu_read_lock();
-    __f_setown(sock->file, priv->_filp->f_owner.pid,
-               priv->_filp->f_owner.pid_type, 1);
+    __f_setown(sock->file, efrm_file_f_owner(priv->_filp)->pid,
+               efrm_file_f_owner(priv->_filp)->pid_type, 1);
     rcu_read_unlock();
   }
 #endif
-  sock->file->f_owner.signum = priv->_filp->f_owner.signum;
+  efrm_file_f_owner(sock->file)->signum =
+                               efrm_file_f_owner(priv->_filp)->signum;
 
   rc = ci_tcp_sync_sockopts_to_os_sock(ni, ep->id, sock);
   put_linux_socket(sock);

src/lib/efthrm/tcp_helper_endpoint_move.c
@@ -15,6 +15,7 @@
 #include <onload/tcp_helper_fns.h>
 #include <onload/version.h>
 #include <onload/oof_interface.h>
+#include "onload_kernel_compat.h"
 
 #if CI_CFG_ENDPOINT_MOVE
 
@@ -417,11 +418,12 @@ int efab_file_move_to_alien_stack(ci_private_t *priv, ci_netif *alien_ni,
   /* Copy F_SETOWN_EX, F_SETSIG to the new file */
 #ifdef F_SETOWN_EX
   rcu_read_lock();
-  __f_setown(old_ep->alien_ref->_filp, priv->_filp->f_owner.pid,
-             priv->_filp->f_owner.pid_type, 1);
+  __f_setown(old_ep->alien_ref->_filp, efrm_file_f_owner(priv->_filp)->pid,
+             efrm_file_f_owner(priv->_filp)->pid_type, 1);
   rcu_read_unlock();
 #endif
-  old_ep->alien_ref->_filp->f_owner.signum = priv->_filp->f_owner.signum;
+  efrm_file_f_owner(old_ep->alien_ref->_filp)->signum =
+                                      efrm_file_f_owner(priv->_filp)->signum;
   old_ep->alien_ref->_filp->f_flags |= priv->_filp->f_flags & O_NONBLOCK;
 
   /********* Point of no return  **********/

src/driver/linux_resource/kernel_compat.sh
@@ -167,6 +167,8 @@ EFRM_HAVE_SET_RXFH_CONTEXT memtype struct_ethtool_ops set_rxfh_context include/l
 
 EFRM_HAVE_EPOLL_PWAIT2 symbol sys_epoll_pwait2 include/linux/syscalls.h
 
+EFRM_F_OWNER_IS_VAL		memtype	struct_file	f_owner	include/linux/fs.h struct fown_struct
+
 # TODO move onload-related stuff from net kernel_compat
 " | grep -E -v -e '^#' -e '^$' | sed 's/[ \t][ \t]*/:/g'
 }

commit a561974c2230d3c73be3fa52d11bb375d1684242

driver/onload: fix file owner usage
OL-Redmine-Id: 14184
Fixes: af4d93c25910 ("ON-16108: kernel compat for f_owner type on 6.12")
Signed-off-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

src/driver/linux_onload/onload_kernel_compat.h
@@ -125,6 +125,33 @@ static inline struct fown_struct* efrm_file_f_owner(struct file *file)
 #endif
 }
 
+
+static inline int
+oo_copy_file_owner(struct file *file_to, struct file *file_from)
+{
+#ifndef EFRM_F_OWNER_IS_VAL
+  /* linux 6.12 */
+  int rc;
+
+  if( efrm_file_f_owner(file_from) == NULL )
+    return 0;
+
+  rc = file_f_owner_allocate(file_to);
+  if( rc != 0 )
+    return rc;
+#endif
+
+  if(efrm_file_f_owner(file_from)->pid != 0) {
+    rcu_read_lock();
+    __f_setown(file_to, efrm_file_f_owner(file_from)->pid,
+               efrm_file_f_owner(file_from)->pid_type, 1);
+    rcu_read_unlock();
+  }
+  efrm_file_f_owner(file_to)->signum = efrm_file_f_owner(file_from)->signum;
+
+  return 0;
+}
+
 #ifdef EFRM_CLOEXEC_FILES_STRUCT
 /* linux 6.12+ */
 #define efrm_close_on_exec close_on_exec

src/driver/linux_onload/ossock_calls.c
@@ -655,14 +655,11 @@ int efab_tcp_helper_create_os_sock(ci_private_t *priv)
   sock = get_linux_socket(ep);
 
   /* Copy F_SETOWN_EX, F_SETSIG to the new file */
-  if(efrm_file_f_owner(priv->_filp)->pid != 0) {
-    rcu_read_lock();
-    __f_setown(sock->file, efrm_file_f_owner(priv->_filp)->pid,
-               efrm_file_f_owner(priv->_filp)->pid_type, 1);
-    rcu_read_unlock();
+  rc = oo_copy_file_owner(sock->file, priv->_filp);
+  if( rc != 0 ) {
+    efab_tcp_helper_destroy_os_sock(priv);
+    return rc;
   }
-  efrm_file_f_owner(sock->file)->signum =
-                               efrm_file_f_owner(priv->_filp)->signum;
 
   rc = ci_tcp_sync_sockopts_to_os_sock(ni, ep->id, sock);
   put_linux_socket(sock);

src/lib/efthrm/tcp_helper_endpoint_move.c
@@ -416,12 +416,10 @@ int efab_file_move_to_alien_stack(ci_private_t *priv, ci_netif *alien_ni,
   new_ep->file_ptr = priv->_filp;
 
   /* Copy F_SETOWN_EX, F_SETSIG to the new file */
-  rcu_read_lock();
-  __f_setown(old_ep->alien_ref->_filp, efrm_file_f_owner(priv->_filp)->pid,
-             efrm_file_f_owner(priv->_filp)->pid_type, 1);
-  rcu_read_unlock();
-  efrm_file_f_owner(old_ep->alien_ref->_filp)->signum =
-                                      efrm_file_f_owner(priv->_filp)->signum;
+  rc = oo_copy_file_owner(old_ep->alien_ref->_filp, priv->_filp);
+  if( rc != 0 )
+    goto fail4;
+
   old_ep->alien_ref->_filp->f_flags |= priv->_filp->f_flags & O_NONBLOCK;
 
   /********* Point of no return  **********/